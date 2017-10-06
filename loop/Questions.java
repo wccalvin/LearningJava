95a71e35119533d2a096e0101c41bb94d94713bc
Clayton William <clayton.calvin@gmail.com>
2017-10-06 19:49:19 -0400

    Add a basic example for while loop

--- loop/Questions.java
+++ loop/Questions.java
@@ -7,7 +7,7 @@ public class Questions {
         String anyQuestions = console.readLine("Are there any questions? ");
         while (anyQuestions.equalsIgnoreCase("yes")) {
             String question = console.readLine("What is your question? ");
-            console.printf("I don't understand, %s\n", question);
+            console.printf("I don't understand... %s\n", question);
             anyQuestions = console.readLine("Are ther any more questions? ");
         }
     }
---
+++ loop/While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String[] args) {
+        System.out.println("Example for while loop ...");
+        int count = 1;
+        while (count < 10) {
+            System.out.println("Current count is " + count);
+            count++;
+        }
+    }
+}
