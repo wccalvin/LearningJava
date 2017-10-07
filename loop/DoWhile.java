2bc8bb6d65e409b0a2bc392bb6f9aa0fb6dc2f15
Clayton William <clayton.calvin@gmail.com>
2017-10-06 20:01:09 -0400

    example for do while loop

---
+++ loop/DoWhile.java
@@ -0,0 +1,9 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int count = 1;
+        do {
+            System.out.println("Count is " + count);
+            count++;
+        } while (count < 10);
+    }
+}
