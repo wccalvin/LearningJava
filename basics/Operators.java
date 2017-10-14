23c452ce03b90799093b08f26a3e886c3e573ba4
Clayton William <clayton.calvin@gmail.com>
2017-10-14 14:44:09 -0400

    example for operators

---
+++ basics/Operators.java
@@ -0,0 +1,13 @@
+// Relational operators: < > <= >=
+// Equality operators: == !=
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println(10 > 5);  // returns true
+        System.out.println(10 == 5);  // returns false
+        System.out.println(10 != 5);  // returns true
+        System.out.println(10 < 5);  // returns false
+        System.out.println(10 >= 5);  // returns true
+        System.out.println(10 <= 5);  // returns false
+    }
+}
