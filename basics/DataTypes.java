fbf6b0f1f35474c6893edd066807ec078fe1c44b
Clayton William <clayton.calvin@gmail.com>
2017-10-14 13:36:51 -0400

    add new examples to basics - data types

---
+++ basics/DataTypes.java
@@ -0,0 +1,20 @@
+// int -> short for integer
+// int data type only allows values between -2,147,483,648 and 2,147,483,647
+// boolean -> that can only be true or false
+// char -> data type is used to represent single characters
+// all char values must be enclosed in single quotes
+
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int integer = 2147483647;  // Example of integer data type
+        boolean isTrue = true;  // Example of boolean data type
+        char firstLetter = 'C';  // Example of char data type
+
+        System.out.println(integer);  // increment the value of integer by one
+                                      // to get an error
+        System.out.println(isTrue);
+        System.out.println(firstLetter);
+
+    }
+}
---
+++ basics/NameA.java
@@ -0,0 +1,5 @@
+public class NameA {
+    public static void main(String[] args) {
+        System.out.println("Clayton William");
+    }
+}
